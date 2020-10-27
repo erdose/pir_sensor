c8bd01b9cd4d9318fbcf3574911b4bce90dc8027
erdose <erdos.attis@gmail.com>
2020-10-27 22:10:50 +0100

    Added request send

---
+++ config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# domoticz configuration
+DOMOTICZ_SERVER_IP = "xxx.xxx.x.xxx"
+DOMOTICZ_SERVER_PORT = "xxxx"
+DOMOTICZ_USERNAME = ""
+DOMOTICZ_PASSWORD = ""
+
+# motion sensor ID
+IDX = 1
--- pir_sensor.py
+++ pir_sensor.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+import config
+import logging
+import requests
 import RPi.GPIO as GPIO
 import time
 
@@ -7,16 +10,34 @@ PIR_PIN = 7
 GPIO.setup(PIR_PIN, GPIO.IN)
 
 def MOTION(PIR_PIN):
-  print (Motion Detected!)
+	url = create_request(config.DOMOTICZ_SERVER_IP, config.DOMOTICZ_SERVER_PORT, config.IDX, 'On')
+	send_to_domoticz(url)
+	print ('Motion Detected!')
 
-print (PIR Module Test (CTRL+C to exit))
-time.sleep(2)
-print (Ready)
+def create_request(server, port, idx, state):
+	url = (
+		f"http://{server}:{port}"
+		f"/json.htm?type=command&param=switchlight&idx={idx}"
+		f"&switchcmd={state}")
+	logging.info(f"The request is {url}")
+	return url
+
+def send_to_domoticz(url):
+	resp = requests.get(url, auth=(config.DOMOTICZ_USERNAME, config.DOMOTICZ_PASSWORD))
+	logging.info(f"The response is {resp}")
+
+logging.basicConfig(filename='loginfo.log', level=logging.INFO)
+logging.info('Start script...')
+logging.info(	f"Input parameters:\r\n"
+				f"Domoticz Server IP: {config.DOMOTICZ_SERVER_IP}\r\n"
+				f"Domoticz Server Port: {config.DOMOTICZ_SERVER_PORT}\r\n"
+				f"Domoticz Server User: {config.DOMOTICZ_USERNAME}\r\n"
+				f"Domoticz Server Password: {config.DOMOTICZ_PASSWORD}")
 
 try:
-  GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
-  while 1:
-    time.sleep(100)
+	GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
+	while 1:
+		time.sleep(100)
 except KeyboardInterrupt:
-  print (Quit)
-  GPIO.cleanup()
+	print ('Quit')
+	GPIO.cleanup()
